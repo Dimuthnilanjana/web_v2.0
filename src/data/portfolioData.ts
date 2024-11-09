34dc498044841be451d0bf26d8f798366319c092
Dimuth Nilanjana <106858400+Dimuthnilanjana@users.noreply.github.com>
2024-11-10 00:13:19 +0530

    add_achivments

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -17,16 +17,18 @@ export const hardcodedData = {
   skills: [
     { _id: "skill1", name: "React", sequence: 1, enabled: true },
     { _id: "skill2", name: "Next.js", sequence: 2, enabled: true },
-    { _id: "skill3", name: "MERN", sequence: 3, enabled: true },
+    { _id: "skill3", name: "SCSS", sequence: 3, enabled: true },
     { _id: "skill4", name: "CSS", sequence: 4, enabled: true },
     { _id: "skill5", name: "Tailwind", sequence: 5, enabled: true },
     { _id: "skill6", name: "Framer Motion", sequence: 6, enabled: true },
     { _id: "skill7", name: "Typescript", sequence: 7, enabled: true },
-    { _id: "skill8", name: "GraphQL", sequence: 8, enabled: true },
+    { _id: "skill8", name: "Figma", sequence: 8, enabled: true },
     { _id: "skill9", name: "Node.js", sequence: 9, enabled: true },
     { _id: "skill10", name: "Express", sequence: 10, enabled: true },
     { _id: "skill11", name: "MongoDB", sequence: 11, enabled: true },
     { _id: "skill12", name: "AWS", sequence: 12, enabled: true },
+    {_id: "skill13", name: "Vue.js", sequence: 13, enabled: true },
+    {_id: "skill14", name: "Angular", sequence: 14, enabled: true },
 
   ],
 
